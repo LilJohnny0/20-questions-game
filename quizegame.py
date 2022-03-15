08255214c5d02e99114922c1ff15628890bf1d6e
John Paul <100666246+LilJohnny0@users.noreply.github.com>
2022-03-14 20:36:47 -0400

    Add files via upload

---
+++ quizegame.py
@@ -0,0 +1,194 @@
+#Test
+print("Welcome to the dungeon!")
+
+playing = input("Ready to play? yes/no ")
+print("remember no capital letters in your answers!")
+print(playing)
+
+if playing != "yes":
+    quit()
+
+print("Goodluck Peasant! ")
+
+#1
+
+answer = input("What does VR stand for? ")
+if answer == "virtual reality":
+    print("Correct moving on!")
+else:
+    print("incorrect, back to the gulag with you!")
+    quit()
+
+#2
+
+answer = input("What does CPU stand for? ")
+if answer == "central processing unit":
+    print("Correct moving on!")
+else:
+    print("incorrect, back to the gulag with you!")
+    quit()
+
+#3
+
+answer = input("How many primary colors are there? ")
+if answer == "3":
+    print("Correct moving on!")
+else:
+    print("incorrect, back to the gulag with you!")
+    quit()
+
+#4
+
+answer = input("Who discovered gravity? ")
+if answer == "issac newton":
+    print("Correct moving on!")
+else:
+    print("incorrect, back to the gulag with you!")
+    quit()
+
+#5
+
+answer = input("When did the first Iphone come out? ")
+if answer == "2007":
+    print("Correct moving on!")
+else:
+    print("incorrect, back to the gulag with you!")
+    quit()
+
+#6
+
+answer = input("When was Telsa founded? ")
+if answer == "2003":
+    print("Correct moving on!")
+else:
+    print("incorrect, back to the gulag with you!")
+    quit()
+
+#7
+
+answer = input("How many questions did you just answer? ")
+if answer == "6":
+    print("Correct moving on!")
+else:
+    print("incorrect, back to the gulag with you!")
+    quit()
+
+#8
+
+answer = input("what year did the first person stand on the moon? ")
+if answer == "1969":
+    print("Correct moving on!")
+else:
+    print("incorrect, back to the gulag with you!")
+    quit()
+
+#9
+
+answer = input("who was the second person to land on the moon? ")
+if answer == "buzz aldrin":
+    print("Correct moving on!")
+else:
+    print("incorrect, back to the gulag with you!")
+    quit()
+
+#10
+
+answer = input("How many days does it take for the Earth to orbit the Sun? ")
+if answer == "365.25":
+    print("Correct moving on!")
+else:
+    print("incorrect, back to the gulag with you!")
+    quit()
+
+#11
+
+answer = input("how many time zones are in russia? ")
+if answer == "11":
+    print("Correct moving on!")
+else:
+    print("incorrect, back to the gulag with you!")
+    quit()
+
+#12
+
+answer = input("what is the capital of russia? ")
+if answer == "moscow":
+    print("Correct moving on!")
+else:
+    print("incorrect, back to the gulag with you!")
+    quit()
+
+#13
+
+answer = input("which language has the most words? ")
+if answer == "english":
+    print("Correct moving on!")
+else:
+    print("incorrect, back to the gulag with you!")
+    quit()
+
+#14
+
+answer = input("what is 9 x 5 + 24? ")
+if answer == "69":
+    print("Correct moving on!")
+else:
+    print("incorrect, back to the gulag with you!")
+    quit()
+
+#15
+
+answer = input("what country has the most islands in the world? ")
+if answer == "sweden":
+    print("Correct moving on!")
+else:
+    print("incorrect, back to the gulag with you!")
+    quit()
+
+#16
+
+answer = input("what country has the highest population? ")
+if answer == "china":
+    print("Correct moving on!")
+else:
+    print("incorrect, back to the gulag with you!")
+    quit()
+
+#17
+
+answer = input("what is the smallest country? (it's also a city) ")
+if answer == "vatican city":
+    print("Correct moving on!")
+else:
+    print("incorrect, back to the gulag with you!")
+    quit()
+
+#18
+
+answer = input("what country has the highest crime rate? ")
+if answer == "venezuela":
+    print("Correct moving on!")
+else:
+    print("incorrect, back to the gulag with you!")
+    quit()
+
+#19
+
+answer = input("what is the most sold video game? ")
+if answer == "minecraft":
+    print("Correct moving on!")
+else:
+    print("incorrect, back to the gulag with you!")
+    quit()
+
+#20
+
+answer = input("what is the most used coding language? (by 80% of developers) ")
+if answer == "python":
+    print("you have completed my quiz, you are very smart!")
+else:
+    print("incorrect on the last question! back to the gulag you go!")
+    quit()
+
+
+
